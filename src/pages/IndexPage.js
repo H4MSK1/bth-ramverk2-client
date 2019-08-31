797c311d57b64f16fe74727eaf52560bd7e2584d
H4MSK1 <alburhan97@gmail.com>
2019-09-01 01:27:25 +0200

    added text

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
 import { DefaultContainer } from '../layouts/DefaultContainer';
 
 const IndexPage = () => (
   <DefaultContainer>
     <Jumbotron>
-      <h1>Welcome to my app</h1>
+      <h1>Välkommen till min me-sida</h1>
       <p>
-        <Button color="success" size="large" onClick={console.log}>
-          Button
-        </Button>
+      Mitt namn är Mohammad Alburhan. Född i Hudiksvall, Hälsingland men uppvuxen i Kristianstad, Skåne.
+      </p>
+      <p>
+        Jag har sedan 14 års åldern varit intresserad & nyfiken på nya teknologier inom webbprogrammerings världen.
+        <br />
+        Mina hobbies är för tillfället webbprogrammering, basket och spel.
       </p>
     </Jumbotron>
   </DefaultContainer>
