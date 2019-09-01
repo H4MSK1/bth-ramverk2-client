f300310483d6714c18b04bc2667a6aeb1fcf681e
H4MSK1 <alburhan97@gmail.com>
2019-09-01 10:21:47 +0200

    ui updates

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Jumbotron } from 'reactstrap';
 import { DefaultContainer } from '../layouts/DefaultContainer';
 
-const PageNotFound = () => (
-  <DefaultContainer>404 Error! Page not found</DefaultContainer>
+const PageNotFound = ({ body }) => (
+  <DefaultContainer>
+    <Jumbotron>
+      <h1 className="display-3">404 Error</h1>
+      {body && (
+        <React.Fragment>
+          <hr className="my-2" />
+          <p>{body}</p>
+        </React.Fragment>
+      )}
+    </Jumbotron>
+  </DefaultContainer>
 );
 
 export default PageNotFound;
