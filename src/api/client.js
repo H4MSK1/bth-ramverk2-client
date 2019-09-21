c7671d5c09177c0c573025f1629674779399131a
H4MSK1 <alburhan97@gmail.com>
2019-09-22 01:34:10 +0200

    update api headers

---
+++ src/api/client.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import TokenService from './TokenService';
+
+const ApiClient = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_URL,
+  headers: { 'Content-Type': 'application/json; charset=utf-8' },
+});
+
+ApiClient.interceptors.request.use(
+  request => {
+    const token = TokenService.getAccessToken();
+    if (token) {
+      request.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return request;
+  },
+  error => Promise.reject(error),
+);
+
+ApiClient.interceptors.response.use(undefined, async error => {
+  const response = error.response;
+  const token = TokenService.getAccessToken();
+  if (
+    token &&
+    response &&
+    response.status === 401 &&
+    error.config &&
+    !error.config.__isRetryRequest
+  ) {
+    TokenService.removeTokens();
+    return window.location.reload();
+  }
+
+  return Promise.reject(error);
+});
+
+export default ApiClient;
