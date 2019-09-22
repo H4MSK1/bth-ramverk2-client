fd3350f0636852160f3e9c25224c5777fe0cb4e2
H4MSK1 <alburhan97@gmail.com>
2019-09-22 23:24:03 +0200

    fetch text for indexpage

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Jumbotron } from 'reactstrap';
 import { DefaultContainer } from 'layouts/DefaultContainer';
+import ApiClient from 'api/client';
 
-const IndexPage = () => (
-  <DefaultContainer>
-    <Jumbotron className="bg-secondary box-shadow">
-      <h2>Välkommen till min me-sida</h2>
-      <p>
-        Mitt namn är Mohammad Alburhan. Född i Hudiksvall, Hälsingland men
-        uppvuxen i Kristianstad, Skåne.
-      </p>
-      <p>
-        Jag har sedan 14 års åldern varit intresserad & nyfiken på nya
-        teknologier inom webbprogrammerings världen.
-        <br />
-        Mina hobbies är för tillfället webbprogrammering, basket och spel.
-      </p>
-    </Jumbotron>
-  </DefaultContainer>
-);
+const IndexPage = () => {
+  const [body, setBody] = React.useState('');
+
+  React.useEffect(() => {
+    ApiClient.get('/')
+      .then(res => {
+        setBody(res.data.data);
+      })
+      .catch(console.error);
+  });
+
+  return (
+    <DefaultContainer>
+      <Jumbotron className="bg-secondary box-shadow">
+        <h2>Välkommen till min me-sida</h2>
+        <p>{body}</p>
+      </Jumbotron>
+    </DefaultContainer>
+  );
+};
 
 export default IndexPage;
